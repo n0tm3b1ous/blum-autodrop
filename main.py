fb888d1875ae7a9f8815e426be4cace886280a8c
n0tm3b1ous <dadaludev@gmail.com>
2024-10-22 14:44:41 +0300

    Updated main script

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import win32api, win32con
 
 tickets = 0
 
-def click(pic):
+def clickOnPic(pic):
     x, y = pyautogui.center(pic)
     win32api.SetCursorPos((x, y))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
@@ -13,8 +13,9 @@ def click(pic):
 def main():
     print("How many tickets do you want to use?")
     tickets = int(input())
-    print("Starting in 5 seconds, swap to your bluestacks window and start the game!")
+    print("Starting in 5 seconds, swap to your bluestacks window and place your cursor on the Play button!")
     time.sleep(5)
+    pyautogui.click()
     print("Dropping...")
     for i in range(tickets):
         timeEnd = time.time() + 30
@@ -23,15 +24,17 @@ def main():
                 bpPic = pyautogui.locateOnScreen("img/blum.png", confidence=0.75) # Blum point recognition confidence
             except pyautogui.ImageNotFoundException:
                 continue
-            click(bpPic)
+            clickOnPic(bpPic)
             # time.sleep(0.01) # delay between clicks
+        if i == tickets-1: break
         time.sleep(1)
         try:
             playButtonPic = pyautogui.locateOnScreen("img/playbutton.png", confidence=0.9)
         except pyautogui.ImageNotFoundException:
             print("Play button not found, exiting!")
             exit(1)
-        click(playButtonPic)
+        clickOnPic(playButtonPic)
+    print("Done!")
         
 if __name__ == "__main__":
     main()
